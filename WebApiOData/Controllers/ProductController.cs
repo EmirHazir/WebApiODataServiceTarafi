545666788e2d7579459c9e2e424e5652beec840d
emircanaziz@gmail.com <emircanaziz@gmail.com>
2017-08-31 06:36:17 +0300

    services is done

---
+++ WebApiOData/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Query;
+using WebApiOData.Models;
+
+namespace WebApiOData.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private NORTHWNDEntities db = new NORTHWNDEntities();
+
+        //Queryable ile içine bu sorguyu sadece OrderBy yapabileceği iznini verdim
+        [Queryable(AllowedQueryOptions =AllowedQueryOptions.OrderBy)]
+        public IQueryable<Product> GetAllProducts()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            return db.Products;
+        }
+    }
+}
---
+++ WebApiOData/Controllers/SupplierController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData.Query;
+using WebApiOData.Models;
+
+namespace WebApiOData.Controllers
+{
+    public class SupplierController : ApiController
+    {
+
+        public SupplierController()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+
+        private NORTHWNDEntities db = new NORTHWNDEntities();
+
+        public List<Supplier> GetAllSuppliers()
+        {
+            List<Supplier> spList = db.Suppliers.ToList();
+            return spList;
+
+        }
+
+        public Supplier GetAllSuppliers(int id)
+        {
+            var spplier = db.Suppliers;
+            return spplier.FirstOrDefault(x => x.SupplierID == id);
+        }
+
+        public List<Supplier> GetAllSupplierOrderByName()
+        {
+            List<Supplier> splits = db.Suppliers.OrderBy(x => x.CompanyName).ToList();
+            return splits;
+        }
+    }
+}
